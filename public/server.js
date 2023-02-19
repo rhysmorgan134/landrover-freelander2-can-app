d955904c5045d18d531fc1d7d4fa11804e8055fb
rhysmorgan134 <36861722+rhysmorgan134@users.noreply.github.com>
2023-02-19 16:08:40 +0000

    fixed parking left and right sensors

--- public/server.js
+++ public/server.js
@@ -19,16 +19,13 @@ module.exports = function (window, dev, mp4Reader) {
     })
 
     const mediumSpeed = new MediumSpeed(io)
-    try {
         const can = require('socketcan');
         const channel = can.createRawChannel("can0", true);
         channel.addListener("onMessage", function ({data, id}) {
             mediumSpeed.parseMessage(data, id);
         });
         channel.start()
-    } catch {
-        console.log("couldnt set up socket can")
-    }
+
 
 
 
